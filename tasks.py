269c4cef75bf8c6b250d16981ab7e4520c54f957
Helena Vermeulen <helenavermeulen@kpnmail.nl>
2025-10-31 15:30:44 +0100

    Did the first assignment

--- tasks.py
+++ tasks.py
@@ -13,7 +13,11 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(num): 
+  if num > 0: 
+    return 1
+  else: 
+    return -1
 
 
 # -----------------------------------------------
@@ -28,9 +32,11 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
-
-
+def ReLu(array, cutoff = 0):
+  indices = np.where(array < cutoff)
+  array[indices] = cutoff
+  return array
+  
 # -----------------------------------------------
 
 
@@ -44,7 +50,8 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
-
+def neural_net_layer(two_dim, one_dim):
+  combination = two_dim * one_dim
+  return reLu(combination)
 
 # ------------------------------------------
